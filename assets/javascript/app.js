df517a47afb50b2e73d81183cced7c976926baed
MondoBiscuit <bradleofabian@gmail.com>
2018-06-08 20:32:18 -0400

    jquery function for replace html added , $ not defined..

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,20 @@
+$(document).ready(function() {
+
 // declare vars//
 
 
 //onclick start timer and switch html//
+
+$(function() {
+
+    $("#startButton").on('click',function() {
+      
+      var content = 'Testing....1,2,3';
+      $('#mainText').replaceWith('<div class="jumbotron bg-info" id="mainText">' + content + '</div>');
+      console.log(content)
+    });
+  
+  });
 
 
 //form function//
@@ -12,3 +25,5 @@
 //win and lose condtions//
 
 //tallly correct and incorrect on html//
+
+});
