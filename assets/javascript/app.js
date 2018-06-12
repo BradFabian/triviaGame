85243e5254b251988b3433d1e3f57fb3e855283f
MondoBiscuit <bradleofabian@gmail.com>
2018-06-12 09:42:29 -0400

    adding questions to Var

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,24 @@ function timerfunc() {
 }
 
 var triviaQuestion1 = "What is the worlds biggest Shark?";
+"How many species of sharks are there?";
+"A: Over 470.";
+
+"Sharks can detect as little as one part per million of what in seawater?";
+"A: Blood.";
+"Sharks are fish that have a skeleton made of what?"
+A: Cartilage.;
+"Sharks typically swim at an average speed of what?"
+A: 5.0 mph.;
+
+ "When attacking, a typical shark can reach what speed?"
+A: 12 mph.;
+"What type of sharks can survive in fresh water?"
+A: Bull shark and River shark.;
+"Before the 16th century sharks were commonly called what?"
+A:  Sea dogs.;
+"How many teeth do sharks lose over their life?"
+A: 30,000 or more.;
 
 
 
