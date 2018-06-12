8cd770eb1c76f8e74613f6abcaf161a8bdca5624
MondoBiscuit <bradleofabian@gmail.com>
2018-06-12 10:52:58 -0400

    Made questions and answers in object var

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,96 @@
+
+/* 
+psuedo code
+ 
+Click to Start "onclick function"
+Timer begins at 60 seconds and countdown "setInterval"
+Player goes through all 10 questions 
+player can only guess one answer per question
+Once completed, player submit's answers "loop/function"
+HTML is updated with users score ".html/.append"
+Score includes: time spent, answers correct, and answers wrong "score function" */
+
+
+//
+
+
+
+
 $(document).ready(function() {
 
 // declare vars//
 var rightScore = 0;
 var wrongScore = 0;
-var timer = setInterval(timerfunc,1000 * 30);
+var timer = setInterval(timerfunc,1000 * 60);
 function timerfunc() {
 
 }
 
-var triviaQuestion1 = "What is the worlds biggest Shark?";
-"How many species of sharks are there?";
-"A: Over 470.";
-
-"Sharks can detect as little as one part per million of what in seawater?";
-"A: Blood.";
-"Sharks are fish that have a skeleton made of what?"
-A: Cartilage.;
-"Sharks typically swim at an average speed of what?"
-A: 5.0 mph.;
-
- "When attacking, a typical shark can reach what speed?"
-A: 12 mph.;
-"What type of sharks can survive in fresh water?"
-A: Bull shark and River shark.;
-"Before the 16th century sharks were commonly called what?"
-A:  Sea dogs.;
-"How many teeth do sharks lose over their life?"
-A: 30,000 or more.;
+var questions = [{
+  ques: "What is the worlds biggest Shark?",
+  ans:["Great White","Hammer Head","Whale Shark","Bull Shark"],
+  name:"bigShark",
+  correct:"Whale Shark",
+  divClass:".whaleShark",
+},
+{
+  ques:"How many species of sharks are there?",
+  ans:  ["470", "800", "280", "80"],
+  name: "species",
+  correct:"470",
+  divClass:".speciesnum",
+},
+{
+  ques:"Sharks can detect as little as one part per million of what in seawater?",
+  ans:["Blood","Motion","Electrical Fields","Sound"],
+  name: "detect",
+  correct:"Blood",
+  divClass:".detectBlood",
+},
+{
+  ques:"Sharks are fish that have a skeleton made of what?",
+  ans: ["Bones", "Cartilage", "Collagen", "Calcium"],
+  name: "skeleton",
+  correct:"Cartilage",
+  divClass:".skeletonMake",
+},
+{
+  ques:"Sharks typically swim at an average speed of what?",
+  ans:["30 mph", "15 mph","5 mph", "10 mph"],
+  name: "speed",
+  correct: "5 mph",
+  divClass: ".speedSwim",
+},
+{
+  ques:"When attacking, a typical shark can reach what speed?",
+  ans:["30 mph", "15 mph","20 mph", "12 mph"],
+  name:"attack",
+  correct:"12 mph",
+  divClass:".attackSpeed",
+ 
+},
+{
+  ques:"What type of shark can survive in fresh water?",
+  ans:["Hammer Head", "Nurse Shark", "Bull Shark", "Black Tip"],
+  name:"survive",
+  correct:"Bull Shark",
+  divClass:".surviveFresh",
+},
+{
+  ques:"Before the 16th century sharks were commonly called what?",
+  ans:["Dark Knights","Sea Dogs","Hell Hounds","Cutthroats"],
+  name:"sharkName",
+  correct:"Sea Dogs",
+  divClass:"sharkDogs",
+ 
+},
+{
+  ques:"How many teeth do sharks lose over their life?",
+  ans:["2,000", "15,000", "5,000", "30,000"],
+  name:"teeth",
+  correct: "30,000",
+  divClass: "teethLoss",
+}] // questions//
 
 
 
@@ -36,7 +100,7 @@ $(function() {
 
     $("#startButton").on('click',function() {
       
-      var content = '<tbody class= "text-white" "id= timer">'+ triviaQuestion1  +'</tbody>';
+      var content = '<tbody class= "text-white" "id= timer">'+ questions  +'</tbody>';
       $('#mainText').replaceWith('<div class="jumbotron bg-info text-white" id="mainText">' + content + '</div>');
       console.log(content)
       $('#timer').html(timer);
