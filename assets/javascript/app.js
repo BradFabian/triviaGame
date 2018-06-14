f083bb95deaad670b470f229b37059ccd109c3e4
MondoBiscuit <bradleofabian@gmail.com>
2018-06-14 14:38:33 -0400

    questions appear! still need better layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,76 @@
-    var questions = [{
-    ques: "What is the worlds biggest Shark?",
-    ans:["Great White","Hammer Head","Whale Shark","Bull Shark"],
-    name:"bigShark",
-    correct:"Whale Shark",
-    divClass:".whaleShark"
-  },
-  {
-    ques:"How many species of sharks are there?",
-    ans:  ["470", "800", "280", "80"],
-    name: "species",
-    correct:"470",
-    divClass:".speciesnum"
-  },
-  {
-    ques:"Sharks can detect as little as one part per million of what in seawater?",
-    ans:["Blood","Motion","Electrical Fields","Sound"],
-    name: "detect",
-    correct:"Blood",
-    divClass:".detectBlood"
-  },
-  {
-    ques:"Sharks are fish that have a skeleton made of what?",
-    ans: ["Bones", "Cartilage", "Collagen", "Calcium"],
-    name: "skeleton",
-    correct:"Cartilage",
-    divClass:".skeletonMake"
-  },
-  {
-    ques:"Sharks typically swim at an average speed of what mph?",
-    ans:["30", "15","5", "10"],
-    name: "speed",
-    correct: "5",
-    divClass: ".speedSwim"
-  },
-  {
-    ques:"When attacking, a typical shark can reach how many mph?",
-    ans:["30", "15","20", "12"],
-    name:"attack",
-    correct:"12",
-    divClass:".attackSpeed"
-   
-  },
-  {
-    ques:"What type of shark can survive in fresh water?",
-    ans:["Hammer Head", "Nurse Shark", "Bull Shark", "Black Tip"],
-    name:"survive",
-    correct:"Bull Shark",
-    divClass:".surviveFresh"
-  },
-  {
-    ques:"Before the 16th century sharks were commonly called what?",
-    ans:["Dark Knights","Sea Dogs","Hell Hounds","Cutthroats"],
-    name:"sharkName",
-    correct:"Sea Dogs",
-    divClass:".sharkDogs"
-   
-  },
-  {
-    ques:"How many teeth do sharks lose over their life?",
-    ans:["2,000", "15,000", "5,000", "30,000"],
-    name:"teeth",
-    correct: "30,000",
-    divClass: ".teethLoss"
-  },
-  {
-    ques:"Sharks have existed for how long?",
-    ans: [ "50 million years", "100,000 years", "300,000 years", "350 million years"],
-    name: "years",
-    correct: "350 million years",
-    divClass:".millionYears"
-  }
-  ]
+var questions = [{
+  ques: "What is the worlds biggest Shark?",
+  ans:["Great White","Hammer Head","Whale Shark","Bull Shark"],
+  name:"bigShark",
+  correct:"Whale Shark",
+  divClass:".whaleShark"
+},
+{
+  ques:"How many species of sharks are there?",
+  ans:  ["470", "800", "280", "80"],
+  name: "species",
+  correct:"470",
+  divClass:".speciesnum"
+},
+{
+  ques:"Sharks can detect as little as one part per million of what in seawater?",
+  ans:["Blood","Motion","Electrical Fields","Sound"],
+  name: "detect",
+  correct:"Blood",
+  divClass:".detectBlood"
+},
+{
+  ques:"Sharks are fish that have a skeleton made of what?",
+  ans: ["Bones", "Cartilage", "Collagen", "Calcium"],
+  name: "skeleton",
+  correct:"Cartilage",
+  divClass:".skeletonMake"
+},
+{
+  ques:"Sharks typically swim at an average speed of what mph?",
+  ans:["30", "15","5", "10"],
+  name: "speed",
+  correct: "5",
+  divClass: ".speedSwim"
+},
+{
+  ques:"When attacking, a typical shark can reach how many mph?",
+  ans:["30", "15","20", "12"],
+  name:"attack",
+  correct:"12",
+  divClass:".attackSpeed"
+ 
+},
+{
+  ques:"What type of shark can survive in fresh water?",
+  ans:["Hammer Head", "Nurse Shark", "Bull Shark", "Black Tip"],
+  name:"survive",
+  correct:"Bull Shark",
+  divClass:".surviveFresh"
+},
+{
+  ques:"Before the 16th century sharks were commonly called what?",
+  ans:["Dark Knights","Sea Dogs","Hell Hounds","Cutthroats"],
+  name:"sharkName",
+  correct:"Sea Dogs",
+  divClass:".sharkDogs"
+ 
+},
+{
+  ques:"How many teeth do sharks lose over their life?",
+  ans:["2,000", "15,000", "5,000", "30,000"],
+  name:"teeth",
+  correct: "30,000",
+  divClass: ".teethLoss"
+},
+{
+  ques:"Sharks have existed for how long?",
+  ans: [ "50 million years", "100,000 years", "300,000 years", "350 million years"],
+  name: "years",
+  correct: "350 million years",
+  divClass:".millionYears"
+}
+]   
   
 
 
@@ -93,7 +93,7 @@ var startGame = $("#startButton").on('click', function() {
 
   // function for displaying questions
 var questionDisplay = function() {
-  $(".questions :not('#sub-but')").empty();
+  $(".questions #questions2 :not('#sub-but')").empty();
 
   // loops through the 10 questions 
   for (var j = 0; j < 10; j++) {
@@ -103,7 +103,7 @@ var questionDisplay = function() {
   for (var i = 0; i < 4; i++) {
       $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
   }
-  $('.questions').prepend('<hr />');
+  /*$('.questions').prepend('<hr />');*/
   }
   }
   
